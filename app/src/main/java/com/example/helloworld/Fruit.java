6d6a037299e56367bcede3daa20f977cec6f49da
Liyux <792885078@qq.com>
2018-11-30 21:04:57 +0800

    Initial commit

---
+++ app/src/main/java/com/example/helloworld/Fruit.java
@@ -0,0 +1,26 @@
+package com.example.helloworld;
+
+/**
+ * Created by ‘。；op on 2018/11/30.
+ */
+
+public class Fruit {
+    private String name;
+    private int ImgId;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getImgId() {
+        return ImgId;
+    }
+
+    public void setImgId(int imgId) {
+        ImgId = imgId;
+    }
+}
